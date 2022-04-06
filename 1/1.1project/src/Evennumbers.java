b307ed5dbb6b821ecc09ef6a65e698eeb3ec14ec
Sandeep <Sk3264537@gmail.com>
2022-04-06 14:35:39 +0530

    first commit

---
+++ 1/1.1project/src/Evennumbers.java
@@ -0,0 +1,24 @@
+
+class EvenNumbers {
+
+  public static void main(String args[]) {
+
+int n = 100;
+
+System.out.print("Even Numbers we need to find from 1 to "+n+" are: ");
+//
+for (int r = 1; r <= n; r++) {
+
+   //if number%2 == 0 it means its an even number
+
+   if (i % 2 == 0) {
+
+ System.out.print(i + " ");
+
+   }
+
+}
+
+  }
+
+}
